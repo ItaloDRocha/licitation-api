80fd553bd11ea8c2b0441249efb3e7da341e085a
italoDRocha <italorocha305@gmail.com>
2024-02-14 18:20:28 -0300

    objeto dos dados funcional

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
+//mandar array de objetos pra api.php
 // import "./api/api.js"
 // // import "./style.css";
 const html_path = "ata_10583.html";
 gravarHTML();
 
-class Licitacao {
-  constructor(dados_array) {
-    (this.item = dados_array[0]),
-      (this.descricao = dados_array[1]),
-      (this.descricao_complementar = dados_array[2]),
-      (this.tratamento_diferenciado = dados_array[3]),
-      (this.quantidade = dados_array[4]),
-      (this.unidade_fornecimento = dados_array[5]),
-      (this.maximo_aceitavel = dados_array[6]),
-      (this.situacao = dados_array[7]),
-      (this.aplicabilidade_decreto = dados_array[8]),
-      (this.aplicabilidade_margem = dados_array[9]),
-      (this.intervalo_lances = dados_array[10]);
-  }
-}
-
 let licitacoes = [];
+
+// class Licitacao {
+//   constructor(dados_array) {
+//     (this.item = dados_array[0]),
+//       (this.descricao = dados_array[1]),
+//       (this.descricao_complementar = dados_array[2]),
+//       (this.tratamento_diferenciado = dados_array[3]),
+//       (this.quantidade = dados_array[4]),
+//       (this.unidade_fornecimento = dados_array[5]),
+//       (this.maximo_aceitavel = dados_array[6]),
+//       (this.situacao = dados_array[7]),
+//       (this.aplicabilidade_decreto = dados_array[8]),
+//       (this.aplicabilidade_margem = dados_array[9]),
+//       (this.intervalo_lances = dados_array[10]);
+//   }
+// }
 
 // let licitacao = new Licitacao();
 // licitacoes.push(licitacao);
@@ -27,29 +28,77 @@ let licitacoes = [];
 async function gravarHTML(html_path) {
   const string_html = await readHtml(html_path);
   $("#app").html(string_html);
-  $("#app table .td").each(function (index, element) {
+  const table_licitacoes = $("#app table").not(".tex3, .td, #loading").first();
+  console.log(table_licitacoes);
+
+  $(".td",table_licitacoes).each(function (index, element) {
     let dados_array = [];
 
-    let item = $("td", element).html();
+    let item = $("td", element).eq(0).text();
+    // console.log("item",item);
     dados_array.push(item);
 
     let dados = $(element).find("td td");
-    console.log(dados);
+    // console.log(dados);
     // throw error();
     // console.log($(dados[0]).text());
     // console.log($(dados[1]).text());
 
-    
-
     $.each(dados, function (index, value) {
-      dados_array.push($(value).text())
+      // console.log($(value).text().trim());
+      dados_array.push($(value).text().trim());
     });
 
     // console.log(dados_array)
-
-    licitacoes.push(new Licitacao(dados_array))
     
 
+    let licitacao_obj = {};
+
+    $.each(dados_array, function (index, value) {
+      switch (index) {
+        case 0:
+          Object.assign(licitacao_obj, {"item": value});
+          break;
+        case 1:
+          Object.assign(licitacao_obj, {"descricao": value});
+          break;
+        case 2:
+          Object.assign(licitacao_obj, {"descricao_complementar": value});
+          break;
+        case 3:
+          Object.assign(licitacao_obj, {"tratamento_diferenciado": value});
+          break;
+        case 4:
+          Object.assign(licitacao_obj, {"quantidade": value});
+          break;
+        case 5:
+          Object.assign(licitacao_obj, {"descricao_complementar": value});
+          break;
+        case 6:
+          Object.assign(licitacao_obj, {"unidade_fornecimento": value});
+          break;
+        case 7:
+          Object.assign(licitacao_obj, {"maximo_aceitavel": value});
+          break;
+        case 8:
+          Object.assign(licitacao_obj, {"situacao": value});
+          break;
+        case 9:
+          Object.assign(licitacao_obj, {"aplicabilidade_decreto": value});
+          break;
+        case 10:
+          Object.assign(licitacao_obj, {"intervalo_lances": value});
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    // console.log(licitacao_obj);
+    
+
+    licitacoes.push(licitacao_obj);
 
     // let descricao = dados[0];
     // console.log(descricao)
@@ -68,7 +117,7 @@ async function gravarHTML(html_path) {
     // console.log($("#app table .td ").eq(0).html());
     
   });
-  console.log(licitacoes)
+  console.log(licitacoes);
 }
 
 function readHtml(html) {
